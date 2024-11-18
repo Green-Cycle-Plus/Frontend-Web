d201b911caafc8bfaf72fb141046284878c00c9d
Wilfred007 <adzerwilfred007@hmail.com>
2024-11-18 10:58:08 +0100

    companies

--- src/app/_components/CompanyCard.tsx
+++ src/app/_components/CompanyCard.tsx
@@ -72,7 +72,7 @@ const CompanyCard = () => {
         <div className=" max-w-[95%] mx-auto grid grid-cols-1 gap-y-8 sm:gap-y-12 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-4 lg:gap-x-8 mt-4 sm:mt-8 px-4 sm:px-8 pb-20">
             {
                 companyCard.map((item, index) => (
-                    <div key={index} className='bg-white border flex justify-center rounded-lg border-1 border-green-950 '>
+                    <div key={index} className='bg-white border flex justify-center rounded-lg border-1 border-[#70B170] '>
                         <div className='w-full'>
                             <div className='bg-gradient-to-r pr-4 from-[#DAFFDA] to-[#237823] h-[80px] rounded-lg flex justify-end w-full'>
                                 <img src={item.rate} height={20} width={20} alt='rate' />
@@ -85,24 +85,24 @@ const CompanyCard = () => {
                             </div>
                             <div className="p-3">
 
-                            <div className='mt-5'>
-                                <h1 className=''>Company: {item.name}</h1>
-                                <h1>Waste Offers: {item.offers}</h1>
-                            </div>
-                            <div className=''>
-                                <p>Location: {item.location}</p>
-                            </div>
-                            <div className='flex gap-5 mt-5'>
-                                <h1 className='bg-gray-100 p-2 rounded-2xl text-gray-600 text-sm'>Glass</h1>
-                                <h1 className='bg-gray-100 p-2 rounded-2xl text-gray-600 text-sm'>Metals</h1>
-                                <h1 className='bg-gray-100 p-2 rounded-2xl text-gray-600 text-sm'>Paper</h1>
+                                <div className='mt-5'>
+                                    <h1 className=''>Company: {item.name}</h1>
+                                    <h1>Waste Offers: {item.offers}</h1>
+                                </div>
+                                <div className=''>
+                                    <p>Location: {item.location}</p>
+                                </div>
+                                <div className='flex gap-5 mt-5'>
+                                    <h1 className='bg-gray-100 p-2 rounded-2xl text-gray-600 text-sm'>Glass</h1>
+                                    <h1 className='bg-gray-100 p-2 rounded-2xl text-gray-600 text-sm'>Metals</h1>
+                                    <h1 className='bg-gray-100 p-2 rounded-2xl text-gray-600 text-sm'>Paper</h1>
 
-                            </div>
-                            <div className='mt-5 flex justify-end'>
-                            <button className="bg-[#228B22] text-white p-[12.8px] rounded-[24px] font-semibold font-dms_sans hover:bg-green-700 transition-colors px-7 py-2 text-sm">
-                                View
-                            </button>
-                            </div>
+                                </div>
+                                <div className='mt-5 flex justify-end'>
+                                    <button className="bg-[#228B22] text-white p-[12.8px] rounded-[24px] font-semibold font-dms_sans hover:bg-green-700 transition-colors px-7 py-2 text-sm">
+                                        View
+                                    </button>
+                                </div>
                             </div>
                         </div>
 
